bd493361e68f352b598f82941bce395c85fa6d52
abdelali fajr <fajr.abdelali@outlook.com>
2024-04-10 02:34:49 +0200

    add the routes to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import './App.css'
-import {Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+import Favorite from './pages/favorite'
+import Checkout from './pages/checkout'
+import NotFound from './pages/notFound'
 
 function App() {
   return (
     <div className="App">
       <div className="content">
         <Routes>
-          
+          <Route path='/' element={<Home />} ></Route>
+          <Route path='/favorite' element={<Favorite />} ></Route>
+          <Route path='/checkout' element={<Checkout />} ></Route>
+          <Route path='*' element={<NotFound />} ></Route>
         </Routes>
       </div>
     </div>
