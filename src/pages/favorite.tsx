30d634e8850a02b8abc516db2501dcdaa6e34c4f
abdelali fajr <fajr.abdelali@outlook.com>
2024-04-10 02:31:51 +0200

    correct the import of productList

---
+++ src/pages/favorite.tsx
@@ -0,0 +1,14 @@
+import ProductList from "../components/shared/productList";
+
+export default function Favorite() {
+
+
+
+
+    return (
+        <>
+            <h1 >favorite</h1>
+            <ProductList />
+        </>
+    )
+}
