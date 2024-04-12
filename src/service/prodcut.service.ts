0ce63fe7143ec36bf640be334dd0b70278769768
abdelali fajr <fajr.abdelali@outlook.com>
2024-04-12 19:36:35 +0200

    add product service

---
+++ src/service/prodcut.service.ts
@@ -0,0 +1,39 @@
+
+const url = "http://localhost:3000/products";
+const products: Product[] = [];
+
+export const fetchData = async (): Promise<Product[]> => {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error ! status ${response.status}`)
+        }
+        return await response.json()
+    } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : "there was an error";
+        console.log(errorMessage);
+        return [];
+    }
+}
+
+export const fetchFavorits = async (): Promise<Product[]> => {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error ! status ${response.status}`);
+        }
+        return await response.json()
+    } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : "there was an error";
+        console.log(errorMessage)
+        return []
+    }
+}
+
+export const fetachBrands = (): string[] => {
+    const brands = products.reduce((acc: string[], elem: Product) => {
+        if (elem.brand !== undefined) acc.includes(elem.brand) ? acc : acc.concat(elem.brand)
+        return acc
+    }, []);
+    return brands
+}
