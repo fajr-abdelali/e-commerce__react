e82460934668ae291d34cff0ae68c0a6d05fc826
abdelali fajr <fajr.abdelali@outlook.com>
2024-06-24 13:33:58 +0200

    icon to skelton

--- src/components/shared/productItem.skelton.tsx
+++ src/components/shared/productItem.skelton.tsx
@@ -1,8 +1,12 @@
+import { PhotoIcon } from "@heroicons/react/24/solid";
+
 export default function ProductItemSkelton() {
     return (
         <div className="w-72 bg-white shadow-sm rounded-sm duration-500 " >
             <div className="animate-pulse">
-                <div className="bg-slate-300 h-80 w-72 object-cover rounded-t-sm" ></div>
+                <div className="bg-slate-300 h-80 w-72 object-cover rounded-t-sm grid place-items-center" >
+                    <PhotoIcon className="stroke-1 h-8 w-8" />
+                </div>
                 <div className=" w-72 pt-2 space-y-3">
                     <div className="grid grid-cols-3 gap-4 gap-x-4">
                         <div className="h-2 bg-slate-300 rounded col-span-2"></div>
